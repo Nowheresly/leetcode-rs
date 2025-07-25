a665cd1c890f405d885fa3ee9236d72cc65949f7
Sylvere Richard <sylvere.richard@gmail.com>
2025-07-25 11:37:47 +0200

    leetcode 3487

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -28,3 +28,4 @@ mod s3304_kth_character;
 mod s3330_possible_string_count;
 mod s3439_max_free_time;
 mod s3440_max_free_time2;
+mod s3487_max_sum;
---
+++ src/solution/s3487_max_sum.rs
@@ -0,0 +1,34 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn max_sum(nums: Vec<i32>) -> i32 {
+        let mut set = std::collections::HashSet::new();
+        let mut max = nums.iter().max().unwrap().clone();
+        for num in nums {
+            if num > 0 {
+                set.insert(num);
+            }
+        }
+        if set.is_empty() {
+            return max;
+        }
+        set.iter().sum::<i32>()
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(7, Solution::max_sum(vec![2, 5]));
+        assert_eq!(1, Solution::max_sum(vec![1,1,0,1,1]));
+        assert_eq!(3, Solution::max_sum(vec![1,2,-1,-2,1,0,-1]));
+        assert_eq!(
+            19,
+            Solution::max_sum(vec![1, 8, 10])
+        );
+    }
+}
